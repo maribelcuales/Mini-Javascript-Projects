5a332ecbd9a629b2b5c393a912d699ee3236d9d7
maribelcuales <maribelcuales@gmail.com>
2021-09-24 17:53:32 +0800

    Added solution to convert kelvin to celsius then fahrenheit.

--- kelvin-weather/App.js
+++ kelvin-weather/App.js
@@ -6,3 +6,17 @@ Recently, Kelvin began publishing his weather forecasts on his website. However 
 With our knowledge of JavaScript, let’s convert Kelvin to Celsius, then to Fahrenheit. 
 **/
 
+// Today's weather forcast 293 degrees kelvin
+const kelvin = 310;
+
+// Convert Kelvin temperature to Celsius
+const celsius = kelvin - 273;
+
+// Convert Celsius to Fahrenheit  
+let fahrenheit = (celsius * (9/5) + 32);
+
+// Round the fahrenheit number using Math.floor() 
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheir.`);  
+// The temperature is 98 degrees Fahrenheir.
