b7c2c3f8f3da242f4c2ca8e75e0709811a0bb6ef
maribelcuales <maribelcuales@gmail.com>
2021-09-24 17:51:17 +0800

    Added project objectives for Kelvin Weather.

---
+++ kelvin-weather/App.js
@@ -0,0 +1,8 @@
+/**
+Deep in his mountain-side meteorology lab, the mad scientist Kelvin has mastered weather prediction.
+
+Recently, Kelvin began publishing his weather forecasts on his website. However there’s a problem: All of his forecasts describe the temperature in Kelvin.
+
+With our knowledge of JavaScript, let’s convert Kelvin to Celsius, then to Fahrenheit. 
+**/
+
