92334aa6aa50b5b2c6f66dc90feea42b2c325566
maribelcuales <maribelcuales@gmail.com>
2021-09-24 18:26:06 +0800

    Added dog years mini project.

---
+++ dog-years/App.js
@@ -0,0 +1,8 @@
+/**  
+Dog Years
+
+Here’s how you convert your age from “human years” to “dog years”:
+
+- The first two years of a dog’s life count as 10.5 dog years each.
+- Each year following equates to 4 dog years.
+**/
